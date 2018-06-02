51b0437ce0903168fbe2d5f1d19e7a3104537b53
Eli <eligolan12@gmaiil.com>
2018-06-02 12:21:14 +0300

    update connection

---
+++ src/EchoServer.java
@@ -0,0 +1,144 @@
+// This file contains material supporting section 3.7 of the textbook:
+// "Object Oriented Software Engineering" and is issued under the open-source
+// license found at www.lloseng.com 
+
+import java.io.*;
+import ocsf.server.*;
+
+/**
+ * This class overrides some of the methods in the abstract 
+ * superclass in order to give more functionality to the server.
+ *
+ * @author Dr Timothy C. Lethbridge
+ * @author Dr Robert Lagani&egrave;re
+ * @author Fran&ccedil;ois B&eacute;langer
+ * @author Paul Holden
+ * @version July 2000
+ */
+public class EchoServer extends AbstractServer 
+{
+	//Class variables *************************************************
+	static mysqlConnection m;
+	private boolean signIn = true;
+	/**
+	 * The default port to listen on.
+	 */
+	final public static int DEFAULT_PORT = 5555;
+
+	//Constructors ****************************************************
+
+	/**
+	 * Constructs an instance of the echo server.
+	 *
+	 * @param port The port number to connect on.
+	 */
+	public EchoServer(int port) 
+	{
+		super(port);
+	}
+
+
+	//Instance methods ************************************************
+
+	/**
+	 * This method handles any messages received from the client.
+	 *
+	 * @param msg The message received from the client.
+	 * @param client The connection from which the message originated.
+	 */
+	public void handleMessageFromClient
+	(Object msg, ConnectionToClient client)
+	{
+		System.out.println("Message received: " + msg + " from " + client);
+		/* check choice */
+		int id = Integer.parseInt(msg.toString());
+		if(id == 1) {
+			signIn = true;
+			return;
+		}
+		else if(id == 2){
+			signIn = false;
+			return;
+		}
+
+		try {
+
+			if(signIn) {
+				if(m.userExist("SELECT * FROM parking WHERE id = " + id + ";")) {	
+					client.sendToClient("SignIn success!");
+				}else {
+					client.sendToClient("SingIn faild!");
+				}
+			}else {
+				if(m.addUserToTable("parking",id)) {
+					client.sendToClient("Register success!");
+				}else {
+					client.sendToClient("Register faild!");
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * This method overrides the one in the superclass.  Called
+	 * when the server starts listening for connections.
+	 */
+	protected void serverStarted()
+	{
+		System.out.println
+		("Server listening for connections on port " + getPort());
+	}
+
+	/**
+	 * This method overrides the one in the superclass.  Called
+	 * when the server stops listening for connections.
+	 */
+	protected void serverStopped()
+	{
+		System.out.println
+		("Server has stopped listening for connections.");
+	}
+
+	//Class methods ***************************************************
+
+	/**
+	 * This method is responsible for the creation of 
+	 * the server instance (there is no UI in this phase).
+	 *
+	 * @param args[0] The port number to listen on.  Defaults to 5555 
+	 *          if no argument is entered.
+	 */
+	public static void main(String[] args) 
+	{
+		int port = 0; //Port to listen on
+		m = new mysqlConnection("jdbc:mysql://cs.telhai.ac.il/studentDB_cs204191357",
+				"cs204191357",
+				"123456");
+
+		// m.printQuery("SELECT * FROM Authors;");
+
+		try
+		{
+			port = Integer.parseInt(args[0]); //Get port from command line
+		}
+		catch(Throwable t)
+		{
+			port = DEFAULT_PORT; //Set port to 5555
+		}
+
+		EchoServer sv = new EchoServer(port);
+
+		try 
+		{
+			sv.listen(); //Start listening for connections
+		} 
+		catch (Exception ex) 
+		{
+			System.out.println("ERROR - Could not listen for clients!");
+		}
+	}
+}
+//End of EchoServer class
---
+++ src/mysqlConnection.java
@@ -0,0 +1,119 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class mysqlConnection {
+	public String url;// = "jdbc:mysql://cs.telhai.ac.il/studentDB_cs204191357";
+	public String user;// = "cs204191357"; 
+	public String pass;// = "123456";
+	public Connection conn;
+	public mysqlConnection(String url, String user, String pass) {
+		super();
+		this.url = url;
+		this.user = user;
+		this.pass = pass;
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			this.conn = DriverManager.getConnection(url,user,pass);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		System.out.println("SQL connection succeed");
+	}
+
+	public void printQuery(String query)
+	{
+		Statement stmt;
+		try 
+		{
+			stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			while(rs.next())
+			{
+				// Print out the values
+				System.out.println(rs.getString(1));
+			} 
+			//rs.close();
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) { /* ignored */}
+			}
+			if (stmt != null) {
+				try {
+					stmt.close();
+				} catch (SQLException e) { /* ignored */}
+			}
+			//stmt.executeUpdate("UPDATE course SET semestr=\"W08\" WHERE num=61309");
+		} catch (SQLException e) {e.printStackTrace();}
+	}
+
+
+	public boolean userExist(String query)
+	{
+		Statement stmt;
+		try 
+		{
+			stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			if(rs.next()) {
+				System.out.println(rs.getString(1));
+				return true;
+			}
+			//rs.close();
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) { /* ignored */}
+			}
+			if (stmt != null) {
+				try {
+					stmt.close();
+				} catch (SQLException e) { /* ignored */}
+			}
+			//stmt.executeUpdate("UPDATE course SET semestr=\"W08\" WHERE num=61309");
+		} catch (SQLException e) {e.printStackTrace();}
+		return false;
+	}
+
+
+	public void createTable(String table){
+		Statement stmt;
+		try {
+			stmt = conn.createStatement();
+			stmt.executeUpdate("create table "+table+"(num int, name VARCHAR(40), semestr VARCHAR(10));");
+			stmt.executeUpdate("load data local infile \"courses.txt\" into table courses");
+
+		} catch (SQLException e) {	e.printStackTrace();}
+
+	}
+
+	public boolean addUserToTable(String table,int id) {
+		try {
+			// add record to table.
+			Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_UPDATABLE);
+			ResultSet uprs;
+
+			uprs = stmt.executeQuery("SELECT * FROM " + table + ";");
+			uprs.moveToInsertRow(); 
+			uprs.updateInt("id",id);
+			uprs.insertRow();
+			return true;
+		}catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+	}
+}
