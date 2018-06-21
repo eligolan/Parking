f238589c384ef14ab8e580de185008aa60466e4e
noabayer <noa.bayer.s2@gmail.com>
2018-06-21 16:55:58 +0300

    manager

--- src/application/ManagerWindowController.java
+++ src/application/ManagerWindowController.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -7,6 +9,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -14,38 +17,93 @@ import javafx.stage.Window;
 
 public class ManagerWindowController {
 
-    @FXML
-    private AnchorPane c1;
+	@FXML
+	private AnchorPane c1;
 
-    @FXML
-    private Button setUpBtn;
+	@FXML
+	private Button setUpBtn;
 
-    @FXML
-    private Button view;
+	@FXML
+	private Button view;
 
-    @FXML
-    void clickOnSetUp(ActionEvent event) {
-    	try {
+	@FXML
+	private Button changePrice;
+
+	@FXML
+	private Button handleComp;
+
+	@FXML
+	private TextField textPrice;
+
+	@FXML
+	void clickHandleComp(ActionEvent event) {
+
+	}
+
+	@FXML
+	void clickOnChangePrice(ActionEvent event) {
+		try {
+		int pricePerHour =Integer.parseInt(textPrice.getText());
+		showMsg(event,"The Change Accept", "new price is: "+pricePerHour);
+		}
+		catch(Exception e)
+		{
+			showMsg(event,"Wrong Input", "try again");
+		}
+
+	}
+
+	@FXML
+	void clickOnSetUp(ActionEvent event) {
+		setWindow(event,"SetUpWin.fxml");
+	}
+
+
+
+
+	@FXML
+	void clickOnView(ActionEvent event) {
+
+	}
+
+	void setWindow(ActionEvent event, String fxmlFile)
+	{
+		try {
 			FXMLLoader fxmloader = new FXMLLoader(getClass().getResource("SetUpWin.fxml")) ;
 			Parent root1 = (Parent) fxmloader.load();
-			 Window existingWindow = ((Node) event.getSource()).getScene().getWindow();
+			Window existingWindow = ((Node) event.getSource()).getScene().getWindow();
 			Stage stage = new Stage();
 			stage.initModality(Modality.APPLICATION_MODAL);
 			stage.initOwner(existingWindow);
-			stage.setTitle("Main Window");
+			stage.setTitle("SetUpWin");
 			stage.setScene(new Scene(root1));
 			stage.show();
 		}catch (Exception e)
 		{
 			e.printStackTrace();
-			System.out.println("couldnt open the MainWindow windows");
+			System.out.println("couldnt open the SetUpWin windows");
 		}
 
-    }
-
-    @FXML
-    void clickOnView(ActionEvent event) {
-
-    }
+	}
+	
+	private void showMsg(ActionEvent event,String text,String smallText) {
+		try {
+			TextEditor.getInstance().setBigText(text);
+			TextEditor.getInstance().setSmallText(smallText);
+			FXMLLoader fxmloader = new FXMLLoader(getClass().getResource("WrongInput.fxml")) ;
+			Parent root1 = (Parent) fxmloader.load();
+			 Window existingWindow = ((Node) event.getSource()).getScene().getWindow();
+			Stage stage = new Stage();
+			stage.initModality(Modality.APPLICATION_MODAL);
+			stage.initOwner(existingWindow);
+			stage.setTitle("WrongInput");
+			stage.setScene(new Scene(root1));
+			
+			stage.show();
+		}catch (Exception e)
+		{
+			System.out.println("couldnt open the WrongInput wondows");
+		}
+	}
 
 }
