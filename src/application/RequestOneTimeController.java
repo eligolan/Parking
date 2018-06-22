c2b2ec9670444b67d69713537fee6ac6f130f5e4
Eli <eligolan12@gmaiil.com>
2018-06-22 16:50:21 +0300

    get order number

--- src/application/RequestOneTimeController.java
+++ src/application/RequestOneTimeController.java
@@ -97,8 +97,11 @@ public class RequestOneTimeController {
 				 System.out.println("failed!");
 			 }
 			 else {
-				 showMsg(event,"Approved",""+getPay(arrive, end));
-				 System.out.println("success!");
+				 snd = new ObjectSender(9,parking_id+" " + customer_id + " " + car_id);
+				 int orderId = (int) ClientServerController.sendMsgToServer(snd);
+				 showMsg(event,"Approved " + orderId,""+getPay(arrive, end));
+				// System.out.println("success!");
+				 
 			 }
 			 //}
 		}
