65518f5d102f78618ff264ab38bdad823c4ac61d
Eli <eligolan12@gmaiil.com>
2018-06-22 22:52:40 +0300

    show compli + register fix

--- src/application/ShowComplaintsController.java
+++ src/application/ShowComplaintsController.java
@@ -1,5 +1,10 @@
 package application;
 
+import java.util.ArrayList;
+
+import ClientServer.ClientServerController;
+import ClientServer.ObjectSender;
+import Logistics.Order;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -7,11 +12,18 @@ import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
 
 public class ShowComplaintsController {
-	
 	@FXML
 	public void initialize()
 	{
-		
+		ObjectSender snd = new ObjectSender(13,"");
+		ArrayList<String> compl = (ArrayList<String>) ClientServerController.sendMsgToServer(snd);
+		String text = "";
+		int i=1;
+		for (String c : compl) {
+			text+=i+") "+ c +"\n";
+			i++;
+		}
+		textCmp.setText(text);
 	}
 
     @FXML
