4279075d5c5fccbc2cf232834b86c341cdde0d6b
noabayer <noa.bayer.s2@gmail.com>
2018-06-23 19:48:30 +0300

    show msg in complaint

--- src/application/SendComplaintController.java
+++ src/application/SendComplaintController.java
@@ -4,10 +4,16 @@ import ClientServer.ClientServerController;
 import ClientServer.ObjectSender;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 
 /**
  * 
@@ -35,8 +41,27 @@ public class SendComplaintController {
     void clickOnSend(ActionEvent event) {
     	String nameSender = TextEditor.getInstance().cst.getName();
     	ObjectSender snd = new ObjectSender(12,nameSender+ ":\n"+ "complaint: " + textComplaint.getText());
+//    	Stage stage = (Stage) send.getScene().getWindow();
+//        stage.close();
     	ClientServerController.sendMsgToServer(snd);
-    	Stage stage = (Stage) send.getScene().getWindow();
-        stage.close();
+    	showMsg(event,"Your massage sent","you will get answer less than 24 hours");
     }
+    
+	private void showMsg(ActionEvent event,String text,String smallText) {
+		try {
+			TextEditor.getInstance().setBigText(text);
+			TextEditor.getInstance().setSmallText(smallText);
+			FXMLLoader fxmloader = new FXMLLoader(getClass().getResource("WrongInput.fxml")) ;
+			Parent root1 = (Parent) fxmloader.load();
+			Window existingWindow = ((Node) event.getSource()).getScene().getWindow();
+			Stage stage = new Stage();
+			stage.initModality(Modality.APPLICATION_MODAL);
+			stage.initOwner(existingWindow);
+			stage.setTitle("Input");
+			stage.setScene(new Scene(root1));
+			stage.show();
+		} catch (Exception e) {
+			System.out.println("couldnt open the WrongInput wondows");
+		}
+	}
 }
